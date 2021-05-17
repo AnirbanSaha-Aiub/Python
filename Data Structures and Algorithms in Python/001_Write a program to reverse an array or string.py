a7e008c57cbcfb248aae362f4429702bfc41233b
Anirban <anirbansaha002@gmail.com>
2021-05-17 14:09:23 +0600

    data structures

---
+++ Data Structures and Algorithms in Python/001_Write a program to reverse an array or string.py
@@ -0,0 +1,28 @@
+## Write a program to reverse an array or string
+# Difficulty Level : Basic
+
+ 
+# Given an array (or string), the task is to reverse the array/string.
+# Examples : 
+ 
+#    Input  : arr[] = {1, 2, 3}
+#    Output : arr[] = {3, 2, 1}
+
+#    Input :  arr[] = {4, 5, 1, 2}
+#    Output : arr[] = {2, 1, 5, 4}
+
+
+# Function to reverse A[] from start to end
+
+def reverseList(A, start, end):
+    while start < end:
+        A[start], A[end] = A[end], A[start]
+        start += 1
+        end -= 1
+
+# Driver function to test above function
+A = [1, 2, 3, 4, 5, 6]
+print(A)
+reverseList(A, 0, (len(A)-1))
+print("Reversed list is")
+print(A)
